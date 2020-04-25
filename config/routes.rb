5034618924f0c858be18c3733a5e3c9035fa9cf5
あたらしまさとら <masatora.atarashi@gmail.com>
2020-04-25 20:03:10 +0900

    make spot controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'spots/index'
+  get 'spots/new'
+  get 'spots/create'
+  get 'spots/update'
+  get 'spots/destroy'
   root 'static_pages#home'
+  namespace :api, {format: 'json'} do
+    resources :spots
+  end
 end
