b289539713010236a6839a56da2529b68244053a
あたらしまさとら <masatora.atarashi@gmail.com>
2020-07-07 03:22:58 +0900

    add signup link for admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_095203) do
+ActiveRecord::Schema.define(version: 2020_07_06_171757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,8 +60,10 @@ ActiveRecord::Schema.define(version: 2020_06_23_095203) do
     t.string "uid"
     t.string "username"
     t.boolean "admin", default: false
+    t.text "tokens"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "spot_comments", "spots"
