cc6b3625e12d0ccc3bcd05d1c53440dcf6751f46
あたらしまさとら <masatora.atarashi@gmail.com>
2020-07-21 03:12:28 +0900

    modify omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -17,10 +17,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
-      sign_in_and_redirect @user, event: :authentication
+      # sign_in_and_redirect @user, event: :authentication
+      render json: @user, event: :authentication
     else
       session["devise.#{provider}_data"] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
+      # redirect_to new_user_registration_url
+      render @user
     end
   end
 
