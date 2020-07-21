14300622677c31485c924c7c379f5d0ed5365944
あたらしまさとら <masatora.atarashi@gmail.com>
2020-07-22 03:40:29 +0900

    modify error

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -22,17 +22,7 @@ class Users::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksCon
   end
 
   def render_data_or_redirect(message, data, user_data = {})
-    if Rails.env.production?
-      if ['inAppBrowser', 'newWindow'].include?(omniauth_window_type)
-        render_data(message, user_data.merge(data))
-      elsif auth_origin_url
-        redirect_to DeviseTokenAuth::Url.generate(auth_origin_url, data.merge(blank: true))
-      else
-        fallback_render data[:error] || 'An error occurred'
-      end
-    else
-      render json: @resource, status: :ok
-    end
+    render json: @resource, status: :ok
   end
 
   def set_email
