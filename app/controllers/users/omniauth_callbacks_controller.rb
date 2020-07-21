3fd7d3be037db165df3939e8bc1d412349a268e1
あたらしまさとら <masatora.atarashi@gmail.com>
2020-07-22 02:17:13 +0900

    Modify

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,8 +2,6 @@
 
 class Users::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksController
   include DeviseTokenAuth::Concerns::SetUserByToken
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
 
   def redirect_callbacks
     super
@@ -21,7 +19,6 @@ class Users::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksCon
   def get_resource_from_auth_hash
     super
     set_email
-    clean_resource
   end
 
   def render_data_or_redirect(message, data, user_data = {})
@@ -34,9 +31,6 @@ class Users::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksCon
         fallback_render data[:error] || 'An error occurred'
       end
     else
-      # @resource.credentials = auth_hash["credentials"]
-
-      # わかりやすい様に開発時はjsonとして結果を返す
       render json: @resource, status: :ok
     end
   end
@@ -45,33 +39,4 @@ class Users::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksCon
     @resource.email = "#{@resource.uid}-#{@resource.provider}@example.com"
   end
 
-  def clean_resource
-    # @resource.name = strip_emoji(@resource.name)
-    # @resource.nickname = strip_emoji(@resource.nickname)
-  end
-
-  def strip_emoji(str)
-    # str.encode('SJIS', 'UTF-8', invalid: :replace, undef: :replace, replace: '').encode('UTF-8')
-  end
-
 end
-  # You should also create an action method in this controller like this:
-  # def twitter
-  #   callback_from :twitter
-  # end
-
-  # private
-  # def callback_from(provider)
-  #   provider = provider.to_s
-
-  #   @user = User.find_for_oauth(request.env['omniauth.auth'])
-
-  #   if @user.persisted?
-  #     flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
-  #     render json: @user, event: :authentication
-
-  #   else
-  #     session["devise.#{provider}_data"] = request.env['omniauth.auth']
-  #     render @user
-  #   end
-  # end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter, Rails.application.credentials.twitter[:twitter_api_key], Rails.application.credentials.twitter[:twitter_api_secret], callback_url: "http://localhost/omniauth/twitter/callback"
+    provider :twitter, Rails.application.credentials.twitter[:twitter_api_key], Rails.application.credentials.twitter[:twitter_api_secret], callback_url: "http://54.168.87.215/omniauth/twitter/callback"
   end
